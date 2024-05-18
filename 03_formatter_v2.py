3c17eb9d8f21628e89cac42e99a56861277dd14a
butlerdtc <butlerr2@middleton.school.nz>
2024-05-18 19:24:01 +1200

    Update

--- 03_formatter_v2.py
+++ 03_formatter_v2.py
@@ -31,4 +31,5 @@ existing_menu = {"Value": {"Beef Burger": 5.69, "Fries": 1, "Fizzy drink": 1},
                  "Super": {"Cheese Burger": 6.69, "Large Fries": 2,
                            "Smoothie": 2}
                  }
-dictionary_formatter(existing_menu)
+result = dictionary_formatter(existing_menu)
+print(result)
